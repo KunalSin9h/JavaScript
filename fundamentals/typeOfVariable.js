95bae52db625b6c01bae429126a8a93ba1308ab5
Kunal Singh <kunalsin9h@gmail.com>
2022-08-12 15:05:53 +0530

    M fundamentals/typofvars

--- fundamentals/typeOfVariable.js
+++ fundamentals/typeOfVariable.js
@@ -5,10 +5,11 @@ console.log(v);
 const u = 19;
 console.log(u);
 
+/*
 var lastName = "Singh";
 console.log(lastName);
-
+*/
 // not using any thing for variable
 
-middleName = "Kumar";
+const middleName = "Kumar";
 console.log(middleName);
